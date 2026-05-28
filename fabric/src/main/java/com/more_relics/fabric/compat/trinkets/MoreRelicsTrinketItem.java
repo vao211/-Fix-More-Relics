69c4c9c19af7454192993ea43712e0aa8d4ae8c4
vao211 <vaolu211@gmail.com>
2026-05-28 07:31:46 +0700

    Fix: Resolve item loss and equip rollbacks in Trinkets integration,compat with ExtraSpellAttribute

--- fabric/src/main/java/com/more_relics/fabric/compat/trinkets/MoreRelicsTrinketItem.java
+++ fabric/src/main/java/com/more_relics/fabric/compat/trinkets/MoreRelicsTrinketItem.java
@@ -14,6 +14,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
+
 public class MoreRelicsTrinketItem extends TrinketItem {
     private AttributeModifiersComponent customAttributes = AttributeModifiersComponent.builder().build();
 
@@ -24,21 +26,34 @@ public class MoreRelicsTrinketItem extends TrinketItem {
         }
     }
 
+    @Override
     public Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, Identifier slotIdentifier) {
-        var defaultModifiers = super.getModifiers(stack, slot, entity, slotIdentifier);
+        Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> uniqueModifiers = ArrayListMultimap.create();
+        try {
+            Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> defaultModifiers = super.getModifiers(stack, slot, entity, slotIdentifier);
+            String itemName = net.minecraft.registry.Registries.ITEM.getId(stack.getItem()).getPath();
+            for (Map.Entry<RegistryEntry<EntityAttribute>, EntityAttributeModifier> entry : defaultModifiers.entries()) {
+                EntityAttributeModifier modifier = entry.getValue();
+                String attributeName = entry.getKey().value().getTranslationKey().replace("attribute.name.", "");
+                String rawPath = slotIdentifier.getPath() + "_" + itemName + "_base_" + attributeName + "_" + modifier.operation().name();
+                String safePath = rawPath.toLowerCase(java.util.Locale.ROOT).replaceAll("[^a-z0-9/._-]", "_");
+                uniqueModifiers.put(entry.getKey(), new EntityAttributeModifier(Identifier.of(slotIdentifier.getNamespace(), safePath), modifier.value(), modifier.operation()));
+            }
 
-        // --- FIX TRINKET STACKING BUG Like RELICS ---
-        Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> mutableModifiers = ArrayListMultimap.create(defaultModifiers);
-        String itemName = net.minecraft.registry.Registries.ITEM.getId(stack.getItem()).getPath();
-
-        for (var entry : this.customAttributes.modifiers()) {
-            Identifier uniqueModId = Identifier.of(slotIdentifier.getNamespace(),
-                    slotIdentifier.getPath() + "_" + itemName + "_" + entry.modifier().id().getPath());
-            mutableModifiers.put(entry.attribute(),
-                    new EntityAttributeModifier(uniqueModId, entry.modifier().value(), entry.modifier().operation()));
+            for (AttributeModifiersComponent.Entry entry : this.customAttributes.modifiers()) {
+                EntityAttributeModifier modifier = entry.modifier();
+                String attributeName = entry.attribute().value().getTranslationKey().replace("attribute.name.", "");
+                String rawPath = slotIdentifier.getPath() + "_" + itemName + "_" + attributeName + "_" + modifier.operation().name();
+                String safePath = rawPath.toLowerCase(java.util.Locale.ROOT).replaceAll("[^a-z0-9/._-]", "_");
+                Identifier uniqueModId = Identifier.of(slotIdentifier.getNamespace(), safePath);
+                uniqueModifiers.put(entry.attribute(),
+                        new EntityAttributeModifier(uniqueModId, modifier.value(), modifier.operation()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return mutableModifiers;
-        // ------------------------------------------------
+
+        return uniqueModifiers;
     }
 
     public void setConfigurableModifiers(AttributeModifiersComponent component) {
